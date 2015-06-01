8b7ee48026e201361c7a57e7d52e200d82856865
10sr <8slashes+git@gmail.com>
2015-06-01 18:19:42 +0900

    Fix formula

--- Formula/erutaso.rb
+++ Formula/erutaso.rb
@@ -4,7 +4,9 @@ class Erutaso < Formula
   homepage 'http://github.com/sgymtic/sl.git'
 
   url 'https://github.com/sgymtic/sl.git', :using => :git,
-      :tag => "ed5b15fd3c68946cff8dee629474f7fe01d67c56"
+      :revision => "ed5b15fd3c68946cff8dee629474f7fe01d67c56"
+
+  version "5.0"
 
   def install
     system 'make'
