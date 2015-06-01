376dfb564a28b1ffed3d5d8a6f67f80aed5391e9
10sr <8slashes+git@gmail.com>
2015-06-01 10:23:10 +0900

    Add pacapt formula

---
+++ Formula/pacapt.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Tor < Formula
+  homepage 'http://github.com/icy/pacapt.git'
+
+  url 'https://github.com/icy/pacapt/archive/v2.0.5.tar.gz'
+  sha256 '2d595f1f794094f5985a1d18ed51bfdfa156ca99f24e7fd5b6e76a9bca86f094'
+
+  def install
+    system 'install' '-Dm' '755' './pacapt' '#{prefix}/bin/pacapt'
+  end
+end
