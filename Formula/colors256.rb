97beb44765cbfd9561ee2703bba0279d70b56c9e
10sr <8slashes+git@gmail.com>
2015-11-20 14:44:47 +0900

    Add formula colors256

---
+++ Formula/colors256.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Colors256 < Formula
+  homepage 'https://gist.github.com/10sr/6852331'
+
+  url 'https://gist.github.com/10sr/6852331.git', :using => :git,
+      :revision => "9d9d50714a4039932bb48091e111378d73c098db"
+
+  version "0.1"
+
+  def install
+    mv "256colors2.pl", "256colors2"
+    bin.install "256colors2"
+  end
+end
