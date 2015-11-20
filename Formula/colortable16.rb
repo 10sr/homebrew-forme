1351fe5d3a143f5b9b55232834b8101f3a344f5a
10sr <8slashes+git@gmail.com>
2015-11-20 14:44:28 +0900

    Fix urls

--- Formula/colortable16.rb
+++ Formula/colortable16.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Colortable16 < Formula
-  homepage 'http://gist.github.com/6852317.git'
+  homepage 'http://gist.github.com/6852317'
 
-  url 'http://gist.github.com/6852317.git', :using => :git,
+  url 'https://gist.github.com/6852317.git', :using => :git,
       :revision => "84d8fc4ce9d3a18f1ccdd81f049abe9b13aff4ba"
 
   version "0.1"
