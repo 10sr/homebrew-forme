7c44789b936a92289be71f77fe86260a29249bbf
10sr <8slashes+git@gmail.com>
2016-04-14 16:09:00 +0900

    Add vis formula

---
+++ Formula/vis.rb
@@ -0,0 +1,18 @@
+class Vis < Formula
+  desc "A vim like text editor"
+  homepage "https://github.com/martanne/vis"
+
+  url "https://github.com/martanne/vis.git",
+      :tag => "v0.2"
+
+  version "0.2"
+
+  depends_on "libtermkey"
+  depends_on "lua"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+end
