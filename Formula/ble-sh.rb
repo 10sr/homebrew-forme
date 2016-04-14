a56a450e9411318807754369398333b4659f105b
10sr <8slashes+git@gmail.com>
2016-04-14 15:53:22 +0900

    Fix ble-sh homepage url

--- Formula/ble-sh.rb
+++ Formula/ble-sh.rb
@@ -1,6 +1,6 @@
 class BleSh < Formula
   desc "Bash Line Editor"
-  homepage "https://github.com/akinomyaga/ble.sh"
+  homepage "https://github.com/akinomyoga/ble.sh"
 
   url "https://github.com/akinomyoga/ble.sh.git",
       :tag => "v0.1.4"
