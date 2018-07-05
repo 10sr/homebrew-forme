122b6b471f766ed66f6eb0fb89dd4d3bea055d3f
10sr <8.slashes@gmail.com>
2018-07-05 13:44:30 +0900

    Fix sha256

--- Formula/sdcv-dict-oxford.rb
+++ Formula/sdcv-dict-oxford.rb
@@ -3,7 +3,7 @@ class SdcvDictOxford < Formula
   homepage "https://sites.google.com/site/gtonguedict/home/stardict-dictionaries"
 
   url "http://downloads.sourceforge.net/xdxf/stardict-comn_dls03_oxford-2.4.2.tar.bz2"
-  sha256 "8b3c38143eb7351fbc816d0d3bd87c08da26e5439a7e93ae53b75af1897e412c"
+  sha256 "1c07bca79aff349fd4d86db658aa5ebe7b6b6fe7b8f90317d1150b585aacb4e5"
 
   def install
     share.install Dir["*"]
